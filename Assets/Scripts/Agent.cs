40e9f42a1992b2770dae60b2ff9a34e13d6bf507
MatheusMacedoDev <matheus.macedo.developer@gmail.com>
2020-06-25 18:37:24 -0300

    Initial graph and agent

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    [SerializeField] private GameObject AgentGameObject;
+    [SerializeField] private Graph Graph;
+    [SerializeField] private float Speed;
+    [SerializeField] private Vector3 Direction;
+    [SerializeField] private GameObject PreviousVertex;
+    [SerializeField] private GameObject ActuallyVertex;
+    private Vector3 Velocity;
+
+    void Awake()
+    {
+        AgentGameObject = this.gameObject;
+        PreviousVertex = null;
+        ActuallyVertex = SearchNearestVertex(Graph.GetVertexsList());
+        Direction = getDirectionToVertex(ActuallyVertex);
+    }
+
+    void Update()
+    {
+        if ((transform.position.x - ActuallyVertex.transform.position.x) < 0.5f && (transform.position.x - ActuallyVertex.transform.position.x) > -0.5f && (transform.position.z - ActuallyVertex.transform.position.z) < 0.5f && (transform.position.z - ActuallyVertex.transform.position.z) > -0.5f)
+        {
+            GameObject actuallyVertex = null;
+
+            foreach (int number in Graph.GetAdjacencyListStartingByVertex(ActuallyVertex))
+            {
+                actuallyVertex = Graph.GetVertexsList()[number];
+
+                if(actuallyVertex != PreviousVertex)
+                {
+                    break;
+                }
+            }
+
+            Direction = (transform.position - actuallyVertex.transform.position).normalized;
+            
+            PreviousVertex = ActuallyVertex;
+            ActuallyVertex = actuallyVertex;
+        }
+        else
+        {
+            Direction = (transform.position - ActuallyVertex.transform.position).normalized;
+            Velocity = Speed * Direction;
+            AgentGameObject.GetComponent<Rigidbody>().velocity = new Vector3(Velocity.x, 0, Velocity.z);
+        }
+    }
+
+    private GameObject SearchNearestVertex(List<GameObject> Vertexs)
+    {
+        GameObject NearestVertex = null;
+        float NearestDistance = 0;
+        bool First = true;
+
+        foreach(GameObject vertex in Vertexs)
+        {
+            if (First)
+            {
+                NearestVertex = vertex;
+                NearestDistance = Vector3.Distance(transform.position, vertex.transform.position);
+                First = false;
+            }
+            else
+            {
+                float distanceVertex = Vector3.Distance(transform.position, vertex.transform.position);
+                if (NearestDistance > distanceVertex)
+                {
+                    NearestVertex = vertex;
+                    NearestDistance = distanceVertex;
+                }
+            }
+        }
+
+        return NearestVertex;
+    }
+
+    private Vector3 getDirectionToVertex(GameObject vertex)
+    {
+        return (transform.position - vertex.transform.position).normalized;
+    } 
+}
---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    [SerializeField] private bool IsWeighted;
+    [SerializeField] private int NumberOfVertexs;
+    [SerializeField] private int MaximumDegree;
+    [SerializeField] private List<GameObject> Vertexs;
+    [SerializeField] private int[,] AdjacencyList;
+    [SerializeField] private int[] Degree;
+    [SerializeField] private float[,] Weights;
+
+    private void Awake()
+    {
+        bool isDigraph = false;
+        AdjacencyList = new int[NumberOfVertexs, MaximumDegree];
+        Degree = new int[NumberOfVertexs];
+        Weights = new float[NumberOfVertexs, MaximumDegree];
+
+        for(int i = 0; i < NumberOfVertexs; i++)
+        {
+            for(int j = 0; j < MaximumDegree; j++)
+            {
+                AdjacencyList[i, j] = -1;
+            }
+        }
+
+        InsertEdge(1, 0, isDigraph, 0);
+        InsertEdge(0, 2, isDigraph, 0);
+        InsertEdge(2, 3, isDigraph, 0);
+        InsertEdge(3, 4, isDigraph, 0);
+
+        PrintGraph();
+    }
+
+    private int InsertEdge(int origin, int destiny, bool isDigraph, float weight)
+    {
+        if (origin < 0 || origin >= NumberOfVertexs || destiny < 0 || destiny >= NumberOfVertexs)
+            return 0;
+
+        AdjacencyList[origin, Degree[origin]] = destiny;
+        if (IsWeighted)
+            Weights[origin, destiny] = weight;
+        Degree[origin]++;
+
+        if (!isDigraph)
+            InsertEdge(destiny, origin, true, weight);
+
+        return 1;
+    }
+
+    private int RemoveEdges(int origin, int destiny, bool isDigraph)
+    {
+        if (origin < 0 || origin >= NumberOfVertexs || destiny < 0 || destiny >= NumberOfVertexs)
+            return 0;
+
+        int i = 0;
+        while (i < Degree[origin] && AdjacencyList[origin, i] != destiny)
+            i++;
+        if (Degree[i] == origin)
+            return 0;
+        Degree[origin]--;
+        AdjacencyList[origin, i] = AdjacencyList[origin, Degree[origin]];
+        if (IsWeighted)
+            Weights[origin, i] = Weights[origin, Degree[origin]];
+        if (!isDigraph)
+            RemoveEdges(destiny, origin, true);
+
+        return 1;
+    }
+
+    private void PrintGraph()
+    {
+        for(int i = 0; i < NumberOfVertexs; i++)
+        {
+            string line = "";
+            for(int j = 0; j < Degree[i]; j++)
+            {
+                if (IsWeighted)
+                    line += AdjacencyList[i, j].ToString() + "(" + Weights[i, j].ToString() + "), ";
+                else
+                    line += AdjacencyList[i, j].ToString() + ", ";
+            }
+            if(line != "")
+            {
+                Debug.Log(i + " -> " + line);
+            }
+            
+        }
+    }
+
+    public int[] GetAdjacencyListStartingByVertex(GameObject _vertex)
+    {
+        int verticeNumber = 0;
+        int[] vertexAdjacencyList;
+
+        foreach(GameObject vertex in Vertexs)
+        {
+            if (vertex == _vertex)
+                break;
+            verticeNumber++;
+        }
+
+        vertexAdjacencyList = new int[Degree[verticeNumber]];
+
+        for(int i = 0; i < Degree[verticeNumber]; i++)
+        {
+            vertexAdjacencyList[i] = AdjacencyList[verticeNumber, i];
+        }
+
+        return vertexAdjacencyList;
+    }
+
+    public GameObject GetVertexStartingByYourNumber(int verticeNumber)
+    {
+        return Vertexs[verticeNumber];
+    }
+
+    public List<GameObject> GetVertexsList()
+    {
+        return Vertexs;
+    }
+}
